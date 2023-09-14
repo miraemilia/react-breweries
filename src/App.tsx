6348ef9e8a3bb7b17afc52f26085e6e9c4478819
Mira Valkama <mira.e.valkama@gmail.com>
2023-09-14 13:37:34 +0300

    fetch single brewery data + status messages for fetching

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,53 @@ import { Brewery } from "./types"
 const App = () => {
 
   const [breweries, setBreweries] = useState<Brewery[]>([])
+  const [singleBreweryId, setSingleBreweryId] = useState<string | undefined>('5128df48-79fc-4f0f-8b52-d06be54d0cec')
+  const [singleBrewery, setSingleBrewery] = useState<Brewery | undefined>(undefined)
+  const [messageAll, setMessageAll] = useState<string>('')
+  const [messageOne, setMessageOne] = useState<string>('')
+
+  const getBreweries = async () => {
+    try {
+      setMessageAll('Fetching all records...')
+      const response = await fetch('https://api.openbrewerydb.org/v1/breweries')
+      if (response.status === 200) {
+        const breweriesJson = await response.json()
+        setBreweries(breweriesJson)
+        setMessageAll('') 
+      } else {
+        setMessageAll('No records')
+      }
+    } catch {
+      setMessageAll('Unable to fetch brewery data')
+    }
+  }
+
+  const getOneBrewery = async () => {
+    try {
+      setMessageOne('Fetching record...')
+      const response = await fetch(`https://api.openbrewerydb.org/v1/breweries/${singleBreweryId}`)
+      console.log(response)
+      if (response.status === 200) {
+        const breweryJson = await response.json()
+        setSingleBrewery(breweryJson)
+        setMessageOne('')
+      } else {
+        setMessageOne('No record')
+      }
+    } catch {
+      setMessageOne('Unable to fetch record')
+    }
+  }
 
   useEffect(() => {
-    const getBreweries = async () => {
-      const response = await fetch('https://api.openbrewerydb.org/v1/breweries')
-      const breweriesJson = await response.json()
-      setBreweries(breweriesJson)
-    }
     getBreweries()
+    getOneBrewery()
   }, [])
 
   return (
     <>
       <Header />
-      <Main breweries={breweries} />
+      <Main breweries={breweries} singleBrewery={singleBrewery} messageAll={messageAll} messageOne={messageOne}/>
       <Footer />
     </>
   )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,12 +3,20 @@ import { BreweryListItem } from "./BreweryListItem"
 
 type MainProps = {
     breweries: Brewery[]
+    singleBrewery: Brewery | undefined
+    messageAll: string
+    messageOne: string
 }
 
-export const Main = ({ breweries } : MainProps) => {
+export const Main = ({ breweries, singleBrewery, messageAll, messageOne } : MainProps) => {
+    console.log(singleBrewery)
   return (
     <div>
+        <h2>Selected brewery</h2>
+        {messageOne ? <p>{messageOne}</p> : <></>}
+        {singleBrewery && <BreweryListItem brewery={singleBrewery}/>}
         <h2>Brewery list</h2>
+        {messageOne ? <p>{messageAll}</p> : <></>}
         {breweries && 
             <div>
                 {breweries.map(b => 
