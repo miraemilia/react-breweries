2642b0227e7cd68259ceb28ef8e759b42aba471f
Mira Valkama <mira.e.valkama@gmail.com>
2023-09-21 13:52:41 +0300

    yup validation and styling for contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,23 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+
+import { ContactForm } from "../types";
 import { Box, Button, FormControl, Input, InputLabel, TextField, Typography } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
-import { SubmitHandler, useForm } from "react-hook-form";
-import { ContactForm } from "../types";
 
 export const Contact = () => {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<ContactForm>()
+    const contactSchema = yup.object({
+        email: yup.string().email().required(),
+        message: yup.string().min(10).max(200).required()
+    }).required()
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<ContactForm>({
+        resolver: yupResolver(contactSchema)
+    })
+
     const onFormSubmit: SubmitHandler<ContactForm> = (data) => console.log(data)
 
     return (
@@ -13,9 +25,26 @@ export const Contact = () => {
             <Typography variant="h2">Contact us</Typography>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <form onSubmit={handleSubmit(onFormSubmit)}>
-                    <FormControl>
-                        <TextField type="email" placeholder="Your email" {...register("email")} variant="standard" sx={{margin: "0.5em", textAlign: "center"}}/>
-                        <TextField fullWidth placeholder="Type your message here" {...register("message")} variant="outlined" sx={{margin: "0.5em", textAlign: "center"}} />
+                    <FormControl sx={{width: "30em"}}>
+                        <TextField 
+                            error={errors.email !== undefined}
+                            placeholder="Your email"
+                            {...register("email")}
+                            helperText={errors.email && (<p>{errors.email.message}</p>)}
+                            variant="standard"
+                            sx={{margin: "0.5em", textAlign: "center", width: "20em"}}
+                        />
+                        <TextField
+                            error={errors.message !== undefined}
+                            placeholder="Type your message here"
+                            {...register("message")}
+                            helperText={errors.message && (<p>{errors.message.message}</p>)}
+                            variant="outlined"
+                            multiline
+                            minRows={2}
+                            maxRows={4}
+                            sx={{margin: "0.5em", textAlign: "center"}}
+                        />
                         <Button type="submit"><SendIcon/> Send</Button>
                     </FormControl>
                 </form>
